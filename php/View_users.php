aa73b76cfc845a3ae558bedf1d30d4a10aeb6aa6
Direccc <112394828+Direccc@users.noreply.github.com>
2024-12-06 00:56:42 +0800

    Final

--- php/View_users.php
+++ php/View_users.php
@@ -30,7 +30,10 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Users</title>
     <link rel="stylesheet" href="/css/View.css">
+    <link rel="stylesheet" href="/css/VFooter.css">
+    <link rel="icon" href="/images/Ash.png" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/index.css"> <!-- Assuming the navigation bar styles are in index.css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -132,6 +135,27 @@ try {
         }
         ?>
     </div>
+
+    <footer class="footer">
+    <div class="Fcontainer">
+        <div class="row">
+            <div class="col-12 text-center">
+                <!-- Brand Name -->
+                <h1 class="brand-name">Direccc</h1>
+
+                <!-- Social Media Icons -->
+                <div class="social-icons">
+                    <a href="https://www.linkedin.com/in/aaron-balolong-071904283/" class="social-icon"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="mailto:aaronbalolong23@gmail.com" class="social-icon"><i class="fas fa-envelope"></i></a>
+                    <a href="https://github.com/Direccc" class="social-icon"><i class="fab fa-github"></i></a>
+                </div>
+
+                <!-- Copyright Notice -->
+                <p class="copyright">© 2024 copyright all rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="/js/View.js"></script>
